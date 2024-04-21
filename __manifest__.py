1fec644252fa02aebb934cca5c7e5a68c108d653
AhmedSaadawy10 <saadawyahmed42@gmail.com>
2024-04-21 19:14:36 +0200

    create hms module

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Hospital Management System",
+    "author": "Ahmed Saadawy",
+    "description": "This is the System for Management Hospital and assignment patients Information",
+    "data": [
+
+    ],
+}
