3c84813b10622b0280c556875b14d77f34509f22
AhmedSaadawy10 <saadawyahmed42@gmail.com>
2024-04-24 05:11:28 +0200

    lab2

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,8 @@
     "author": "Ahmed Saadawy",
     "description": "This is the System for Management Hospital and assignment patients Information",
     "data": [
+        "views/hms_department_view.xml",
+        "views/hms_doctor_view.xml",
         "views/hms_patient_view.xml",
 
     ],
--- models/patient.py
+++ models/patient.py
@@ -1,12 +1,12 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Patient(models.Model):
     _name = "hms.patient"
     _rec_name = "f_name"
 
-    f_name = fields.Char(string="First Name")
-    l_name = fields.Char(string="Last Name")
+    f_name = fields.Char(string="First Name", required=True)
+    l_name = fields.Char(string="Last Name", required=True)
     birth_date = fields.Date(string="Birth Date")
     history = fields.Html(string="History")
     cr_ratio = fields.Float(string="CR Ratio")
@@ -24,7 +24,36 @@ class Patient(models.Model):
     image = fields.Binary(string="Image")
     address = fields.Text(string="Address")
     age = fields.Integer(string="Age")
+    department_id = fields.Many2one("hms.department")
+    department_capacity = fields.Integer(related="department_id.capacity")
+    doctors_ids = fields.Many2many("hms.doctor")
+    log_history_ids = fields.One2many("patient.log.history", "patient_id")
+    state = fields.Selection([
+        ('undetermined', "Undetermined"),
+        ('good', "Good"),
+        ('fair', "Fair"),
+        ('serious', "Serious")
+    ], default='good')
 
 
+@api.onchange("age")
+def on_change_age(self):
+    if self.age and self.age < 30:
+        self.pcr = True
+        return {
+            "warning": {
+                "title": "Note",
+                "message": "The pcr is Checked because the age is less than 30",
+                "type": "Notification"
+            }
+        }
+    else:
+        self.pcr = False
 
 
+class PatientLogHistory(models.Model):
+    _name = "patient.log.history"
+
+    patient_id = fields.Many2one("hms.patient", readonly=True)
+    current_date = fields.Datetime(related="patient_id.create_date")
+    description = fields.Text()
