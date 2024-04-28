92aa311b0d034bd395b5f31a9efb6040b86af290
AhmedSaadawy10 <saadawyahmed42@gmail.com>
2024-04-29 02:56:41 +0300

    lab4

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,9 @@
     "author": "Ahmed Saadawy",
     "description": "This is the System for Management Hospital and assignment patients Information",
     "data": [
+        "reports/patient_print.xml",
+        "security/hms_security_view.xml",
+        "security/ir.model.access.csv",
         "views/hms_customer_view.xml",
         "views/hms_department_view.xml",
         "views/hms_doctor_view.xml",
